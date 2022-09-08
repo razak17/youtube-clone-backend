d0175544afe7ad6ac7159d14ded627073d89bce3
Razak <moabdulrazak17@gmail.com>
2022-09-08 13:34:45 +0000

    chore(auth): ignore __v in login response

--- src/utils/omit.ts
+++ src/utils/omit.ts
@@ -1,6 +1,6 @@
-function omit<T>(obj: T, property: keyof T | (keyof T)[]) {
+function omit<T extends object>(obj: T, property: keyof T | (keyof T)[]) {
   if (Array.isArray(property)) {
-    const entries = Object.entries(obj as string[]).filter((item) => {
+    const entries = Object.entries(obj).filter((item) => {
       const [key] = item;
 
       return !property.includes(key as keyof T);
